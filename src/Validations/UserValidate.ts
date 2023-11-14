97a1b0953286d056627a054ec73d62363476af08
orprogrammer <orprogrammer@gmail.com>
2023-11-14 19:42:25 +0430

    refactor user validation

--- src/Validations/UserValidate.ts
+++ src/Validations/UserValidate.ts
@@ -1,5 +1,20 @@
 
 import Joi from 'joi';
+const emailValidate = Joi.string()
+    .email()
+    .required()
+    .messages({
+        'string.base': 'ایمیل باید یک رشته باشد',
+        'string.email': 'ایمیل باید یک آدرس ایمیل معتبر باشد',
+        'any.required': 'ایمیل اجباری است',
+    });
+const passwordValidation = Joi.string().min(4).max(255).required()
+    .messages({
+        'string.min': 'رمز عبور باید حداقل ۴ کاراکتر باشد',
+        'string.max': 'رمز عبور نمی‌تواند بیشتر از ۲۵۵ کاراکتر باشد',
+        'any.required': 'رمز عبور اجباری است',
+    });
+
 export const userValidate = Joi.object({
     name: Joi.string().required().min(4).max(255)
         .messages({
@@ -7,20 +22,8 @@ export const userValidate = Joi.object({
             'string.min': 'نام کامل باید حداقل ۴ کاراکتر باشد',
             'string.max': 'نام کامل نمی‌تواند بیشتر از ۲۵۵ کاراکتر باشد',
         }),
-    email: Joi.string()
-        .email()
-        .required()
-        .messages({
-            'string.base': 'ایمیل باید یک رشته باشد',
-            'string.email': 'ایمیل باید یک آدرس ایمیل معتبر باشد',
-            'any.required': 'ایمیل اجباری است',
-        }),
-    password: Joi.string().min(4).max(255).required()
-        .messages({
-            'string.min': 'رمز عبور باید حداقل ۴ کاراکتر باشد',
-            'string.max': 'رمز عبور نمی‌تواند بیشتر از ۲۵۵ کاراکتر باشد',
-            'any.required': 'رمز عبور اجباری است',
-        }),
+    email: emailValidate,
+    password: passwordValidation,
 
     confirmPassword: Joi.string().valid(Joi.ref('password')).required().label('confirmPassword')
         .messages({
@@ -29,3 +32,9 @@ export const userValidate = Joi.object({
         })
 
 });
+
+
+export const login = Joi.object({
+    email: emailValidate,
+    password: passwordValidation,
+})
